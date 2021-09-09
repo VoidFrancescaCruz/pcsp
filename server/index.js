3c8278821d7ae901013e23e7538c7432169039b2
Francesca Cruz <70466079+VoidFrancescaCruz@users.noreply.github.com>
2021-09-09 10:22:17 +0800

    Uploaded file limit is 50mb

--- server/index.js
+++ server/index.js
@@ -8,8 +8,10 @@ import postRoutes from './routes/posts.js'
 const app = express();
 
 
-app.use(express.urlencoded({extended: true}))
-app.use(express.json())
+//app.use(express.urlencoded({extended: true}))
+//app.use(express.json())
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(cors())
 
 app.use('/posts', postRoutes);
